a5210fb5004c52c66e774bfbec32242d234da456
epdiegoo <die.castrob@duocuc.cl>
2026-05-09 03:23:15 -0400

    correcion

--- auth/src/test/java/com/donaton/auth/ApplicationTests.java
+++ auth/src/test/java/com/donaton/auth/ApplicationTests.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
-@ActiveProfiles("test")
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
-
+		assertTrue(true);
 	}
-
 }
